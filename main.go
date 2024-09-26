fa8bf961c37d190925ec8dc5f0aca254a3abb91f
Thomas.Elsgaard <thomas.elsgaard@mooserocket.dk>
2024-09-26 11:24:28 +0200

    Added startup log

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ package main
 import (
 	"github.com/truecommercedk/f5ltm_exporter/config"
 	"github.com/truecommercedk/f5ltm_exporter/prober"
+	"log/slog"
 	"maragu.dev/env"
+	"net"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	_ = env.Load()
+
+	host := env.GetStringOrDefault("HOST", "0.0.0.0")
+	port := env.GetIntOrDefault("PORT", 9143)
+
+	address := net.JoinHostPort(host, strconv.Itoa(port))
 
 	envConfig := config.Config{
 		F5User: env.GetStringOrDefault("F5_USER", ""),
@@ -22,6 +30,11 @@ func main() {
 	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
 		prober.Handler(w, r, envConfig)
 	})
-	http.ListenAndServe(":9143", nil)
+
+	slog.Info("F5 Local Traffic Management Device Exporter Starting")
+
+	if err := http.ListenAndServe(address, nil); err != nil {
+		slog.Error("Error starting server")
+	}
 
 }
